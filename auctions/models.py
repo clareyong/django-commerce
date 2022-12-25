02ac85588eae98cfc0db5a468ebfd01b76796e09
Clare Yong <claremagdelene@gmail.com>
2022-12-25 13:53:00 +0800

    Add listing

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    seller = models.ForeignKey("User", on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=64)
+    price = models.BigIntegerField()
+
+    def __str__(self):
+        return f"{self.item_name}"
+
+# class Listing():
+#     pass
+
+# class Bids():
+#     pass
